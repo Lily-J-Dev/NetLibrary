5fea7b49f0c9334ad100750af57532cfc0998f44
Toby <Toby>
2020-02-17 18:02:07 +0000

    Fixed an issue causing the ip to always be localhost on the test app

--- Source/TestClient.cpp
+++ Source/TestClient.cpp
@@ -7,9 +7,9 @@ TestClient::TestClient()
     std::string input = "";
     std::getline(std::cin, input);
     std::getline(std::cin, input);
-    if(input == "localhost" || "")
+    if(input == "localhost" || input == "")
         input = "127.0.0.1";
-    client.ConnectToIP(input, 24000);
+    client.ConnectToIP(input, 24001);
 }
 
 int TestClient::Update()
