a455df8c42094a1e09b38e5ea94af6fac4740b09
Toby <Toby>
2020-02-19 23:15:15 +0000

    Revert "Adding an ifdef for server.h since cmake can't find it for some reason."
    This reverts commit 5d23e1df4dabc5d40e670ce207ea542888de55a4.

--- Headers/ServerConnection.h
+++ Headers/ServerConnection.h
@@ -1,11 +1,7 @@
 #ifndef CTP_SERVERCONNECTION_H
 #define CTP_SERVERCONNECTION_H
 
-#ifdef WIN32
-    #include "Windows/Server.h"
-#elif
-    #include "Linux/Server.h"
-#endif
+#include "Server.h"
 #include "NetworkDevice.h"
 #include "Lobby.h"
 
